a3f482ae0a76b59249cf4906ff9c45b1f1be6849
wengmister <wengmister@gmail.com>
2024-10-10 18:31:25 -0500

    updated ID3 build

--- ID3.py
+++ ID3.py
@@ -1,9 +1,9 @@
 from node import Node
 import math
 import parse
-from seperate import *
+from separate import *
 
-def ID3(examples, default):
+def ID3(examples: dict, default):
   '''
   Takes in an array of examples, and returns a tree (an instance of Node) 
   trained on the examples.  Each example is a dictionary of attribute:value pairs,
@@ -16,14 +16,31 @@ def ID3(examples, default):
   if classes.count(classes[0]) == len(classes):
       return Node(label=classes[0], is_leaf=True)
 
-  # Step 2: If no attributes are left to split, return a leaf node with the majority class
+  attributes = set(examples.keys())
+
+  # If no attributes are left to split, return a leaf node with the majority class
   if len(attributes) == 0:
       majority_class = max(set(classes), key=classes.count)
       return Node(label=majority_class, is_leaf=True)
 
-  # Step 3: Find the best attribute to split on (we'll use information gain)
-  best_attribute = choose_best_attribute(data, attributes)
+  # Find the best attribute to split on (we'll use information gain)
+  best_attribute = find_best_attribute_to_split_on(examples)
   root = Node(attribute=best_attribute)
+
+  # For each value of the best attribute, create a subtree
+  attribute_values = set(row[best_attribute] for row in examples)
+  for value in attribute_values:
+      subset = [row for row in examples if row[best_attribute] == value]
+      
+      # Remove the used attribute and recursively build child nodes
+      new_attributes = [attr for attr in attributes if attr != best_attribute]
+      child = ID3(subset, new_attributes)
+      
+      # Add the child node to the root
+      root.add_child(value, child)
+
+  return root
+
 
 def prune(node, examples):
   '''
@@ -45,10 +62,6 @@ def evaluate(node, example):
   assigns to the example.
   '''
   pass
-
-
-def information_gain(h0, h1):
-  return h0 - h1
 
 
 def main():
